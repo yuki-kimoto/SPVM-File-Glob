af1b0fe2d32a685f2325eb6c059bceadd1fdd728
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-15 10:50:28 +0900

    Added Perl's bsd_glob codes

--- lib/SPVM/File/Glob.c
+++ lib/SPVM/File/Glob.c
@@ -1,6 +1,7 @@
 #include "spvm_native.h"
 
-
+#include "bsd_glob.h"
+#include "bsd_glob.c"
 
 static const char* FILE_NAME = "File/Glob.c";
 
@@ -11,4 +12,38 @@ int32_t SPVM__File__Glob__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+/*
 
+static void
+doglob(pTHX_ const char *pattern, int flags)
+{
+    dSP;
+    glob_t pglob;
+    int i;
+    int retval;
+    SV *tmp;
+    {
+	dMY_CXT;
+
+	/* call glob */
+	memset(&pglob, 0, sizeof(glob_t));
+	retval = bsd_glob(pattern, flags, errfunc, &pglob);
+	GLOB_ERROR = retval;
+
+	/* return any matches found */
+	EXTEND(sp, pglob.gl_pathc);
+	for (i = 0; i < pglob.gl_pathc; i++) {
+	    /* printf("# bsd_glob: %s\n", pglob.gl_pathv[i]); */
+	    tmp = newSVpvn_flags(pglob.gl_pathv[i], strlen(pglob.gl_pathv[i]),
+				 SVs_TEMP);
+	    TAINT;
+	    SvTAINT(tmp);
+	    PUSHs(tmp);
+	}
+	PUTBACK;
+
+	bsd_globfree(&pglob);
+    }
+}
+
+*/
