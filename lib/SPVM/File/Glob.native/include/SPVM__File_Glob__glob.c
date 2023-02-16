e444d5199237264f6bd638ac531db6644f6e1d1e
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-16 10:05:31 +0900

    Fixed Windows lstat bug

--- lib/SPVM/File/Glob.native/include/SPVM__File_Glob__glob.c
+++ lib/SPVM/File/Glob.native/include/SPVM__File_Glob__glob.c
@@ -1080,7 +1080,11 @@ g_lstat(Char *fn, struct stat *sb, glob_t *pglob)
 		return(-1);
 	if (pglob->gl_flags & GLOB_ALTDIRFUNC)
 		return((*pglob->gl_lstat)(buf, sb));
+#if defined(_WIN32)
+	return(stat(buf, sb));
+#else
 	return(lstat(buf, sb));
+#endif
 }
 
 static int
